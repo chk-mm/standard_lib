ca6b6f423c39602e226091b772db0f972e49e0d8
Chonnakarn Rungjaroenporn <chonnak.rung@gmail.com>
2023-09-25 17:20:35 +0700

    task 3 iso

--- task3.py
+++ task3.py
@@ -1,15 +1,14 @@
 import requests
 from pprint import pprint
 import datetime
+from datetime import datetime as dt
 from collections import Counter
 
 
 url = 'https://api.open-meteo.com/v1/forecast?latitude=13.754&longitude=100.5014&daily=temperature_2m_max&forecast_days=14'
 response = requests.get(url)
-date_predict = datetime.datetime.now()  + datetime.timedelta(days=7)
-print(date_predict)
+date_predict = dt.now()  + datetime.timedelta(days=7)
 values_of_prediction =  response.json()
-print(values_of_prediction['daily']['temperature_2m_max'])
 max_weather = 0
 
 for x in values_of_prediction['daily']['temperature_2m_max']:
@@ -17,8 +16,10 @@ for x in values_of_prediction['daily']['temperature_2m_max']:
         max_weather = x
 
 indexofday = values_of_prediction['daily']['temperature_2m_max'].index(max_weather)
-# test = values_of_prediction['daily']['time'][indexofday]
-# print(max_weather,values_of_prediction['daily']['time'][indexofday])
-# datetime.fromisoformat(test)
-print(indexofday.fromisoformat())
-# pprint(response.json())
+test = values_of_prediction['daily']['time'][indexofday]
+txt_to_datetime = test.split('-')
+date_format = dt(int(txt_to_datetime[0]),int(txt_to_datetime[1]),int(txt_to_datetime[2]))
+as_iso = date_format.isoformat()
+
+hottest_date = dt.fromisoformat(as_iso)
+print(hottest_date," is the hottest date which temp is:",max_weather)
